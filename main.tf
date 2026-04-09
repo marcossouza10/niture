24e10d2b551986cb5d72ced9e5c0c7560f8f67a2
Marcos Souza <marcossouza2005@yahoo.com.br>
2026-04-09 11:48:06 -0300

    correcao arquivo main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   user_ocid = var.user_ocid
   fingerprint = var.fingerprint
-  private_key = var.private_key
+  private_key = replace(var.private_key, "\\n", "\n")
   region = var.region
 }
 
